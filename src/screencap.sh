13ec639d83a8b1c0d2652c3463cd3dd0c602cd7b
David Wildasin <dragonmaus@posteo.net>
2020-04-13 12:37:44 -0400

    Clean up scripts

--- src/screencap.sh
+++ src/screencap.sh
@@ -4,11 +4,11 @@ window=
 while getopts :fr opt
 do
   case $opt in
-  (f) window="$( xdotool getwindowfocus -f )" ;;
-  (r) window=root                             ;;
-  (*) break                                   ;;
+  (f) window=$(xdotool getwindowfocus -f) ;;
+  (r) window=root                         ;;
+  (*) break                               ;;
   esac
 done
-shift $(( OPTIND - 1 ))
+shift $((OPTIND - 1))
 
-exec gm import ${window:+-window "$window"} "$@" "$HOME/desk/pictures/screenshots/$( date -u +%FT%TZ ).png"
+exec gm import ${window:+-window "$window"} "$@" ~/desk/pictures/screenshots/$(date -u +%FT%TZ).png
