617734a268b454a69c13174981ae2190518dd3fe
David Wildasin <dragonmaus@posteo.net>
2020-04-25 23:09:10 -0400

    Move randomise-wallpaper.sh and set-wallpaper.sh to master

---
+++ src/randomise-wallpaper.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd "$(xdg-user-dir BACKGROUNDS)"
+
+current=$(readlink .current)
+
+random=$current
+while [[ "$random" = "$current" ]]
+do
+  random=$(find -L .current-res -type f 2> /dev/null | shuf | head -1)
+done
+
+ln -fns "$random" .current
+
+[[ "$1" = -s ]] && exec set-wallpaper .current
